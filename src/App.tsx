5a469327a52b2980c9b9d9288c8981b1454afafd
Alexander.D <74902666+lonegdd@users.noreply.github.com>
2023-04-25 22:39:52 +0300

    feat(invoices) - added invoices grid

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import {Route, Routes} from 'react-router-dom';
 import {Sidebar} from "./scenes/global/Sidebar";
 import {Team} from "./scenes/teams";
 import {Contacts} from "./scenes/contacts/contacts";
+import {Invoices} from "./scenes/Invoices";
 
 function App() {
     const [theme, colorMode] = useMode()
@@ -23,7 +24,7 @@ function App() {
                             <Route path="/" element={<Dashboard/>}/>
                             <Route path="/team" element={<Team />} />
                             <Route path="/contacts" element={<Contacts />} />
-                            {/*<Route path="/invoices" element={<Invoices />} />*/}
+                            <Route path="/invoices" element={<Invoices />} />
                             {/*<Route path="/form" element={<Form />} />*/}
                             {/*<Route path="/bar" element={<Bar />} />*/}
                             {/*<Route path="/pie" element={<Pie />} />*/}
