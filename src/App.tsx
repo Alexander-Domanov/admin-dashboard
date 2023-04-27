fd491aa7da1696b68221acc17de708a0ac25aa36
Alexander.D <74902666+lonegdd@users.noreply.github.com>
2023-04-27 18:17:20 +0300

    feat(LineChart) - add PieChart

--- src/App.tsx
+++ src/App.tsx
@@ -9,9 +9,10 @@ import {Team} from "./scenes/teams";
 import {Contacts} from "./scenes/contacts/contacts";
 import {Invoices} from "./scenes/Invoices";
 import {Form} from "./scenes/form";
-import {Bar} from "./scenes/Bar";
+import {Bar} from "./scenes/bar";
 import {Geography} from "./scenes/geography";
 import {Line} from "./scenes/line";
+import {Pie} from "./scenes/pie";
 
 function App() {
     const [theme, colorMode] = useMode()
@@ -31,7 +32,7 @@ function App() {
                             <Route path="/invoices" element={<Invoices />} />
                             <Route path="/form" element={<Form />} />
                             <Route path="/bar" element={<Bar />} />
-                            {/*<Route path="/pie" element={<Pie />} />*/}
+                            <Route path="/pie" element={<Pie />} />
                             <Route path="/line" element={<Line />} />
                             {/*<Route path="/faq" element={<FAQ />} />*/}
                             {/*<Route path="/calendar" element={<Calendar />} />*/}
---
+++ src/scenes/pie/index.tsx
@@ -0,0 +1,14 @@
+import { Box } from "@mui/material";
+import {PieChart} from "../../components/PieChart";
+import {Header} from "../../components/Header";
+
+export const Pie = () => {
+    return (
+        <Box m="20px">
+            <Header title="Pie Chart" subtitle="Simple Pie Chart" />
+            <Box height="75vh">
+                <PieChart />
+            </Box>
+        </Box>
+    );
+};
